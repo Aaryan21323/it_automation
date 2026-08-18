0fd46092bdafdfc176c6ee6e5e87bfad0c297e09
Aaryan Bista <aaryanbista31@gmail.com>
2026-08-18 20:22:49 +0545

    Git Commands

---
+++ module3/gitcommand.py
@@ -0,0 +1,9 @@
+# git init
+# git status
+# git add 
+# git commit 
+# git log
+# git config -i
+# git config --global user.name "Aaryan"
+# git config --global user.email "aaryanbista31@gmail.com"
+# git diff
