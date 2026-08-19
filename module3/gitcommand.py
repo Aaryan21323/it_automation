f121329cd0b737c5d78111bc97d62cee8a095227
Aaryan Bista <aaryanbista31@gmail.com>
2026-08-19 20:42:26 +0545

    Added new commands

--- module3/gitcommand.py
+++ module3/gitcommand.py
@@ -6,4 +6,24 @@
 # git config -i
 # git config --global user.name "Aaryan"
 # git config --global user.email "aaryanbista31@gmail.com"
-# git diff
+# git diff (shows difference)
+# git commit -a (directly add to directory without staging)
+# git log -p ( shows log with patch)
+# git show commit_id (shows specific commit using the commit_id)
+# git log --stat (shows stat)
+# git rm (delete file)
+# git mv (rename file)
+# git checkout (revert)
+# git checkout -p
+# git reset HEAD (opposite of add , undo from staging area)
+# git reset -p (patch version)
+# git commit --ammend (overwrite the previous commit)
+# git revert HEAD (rollback)
+# git log -p -2
+# git branch (show branches)
+# git branch branch_name (create new branch)
+# git branch checkout branch_name (switch branch)
+# git log -2
+# git merge branch_name (merge branches)
+# git log --graph --oneline (shows branch graph)
+# git merge --abort (stop merge)
